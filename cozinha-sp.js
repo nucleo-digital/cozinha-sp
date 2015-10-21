a904c0d7e85290e7d5ce7c6ec0c21b3e64e5875c
Lucas Pirola <lucaspirola@gmail.com>
2015-10-21 02:00:29 -0200

    Adiciona validação no formulário de doação.
    close #2

--- cozinha-sp.js
+++ cozinha-sp.js
@@ -3,36 +3,36 @@ var INITIAL_INTERVAL = 60;
 var META_TOTAL = 1000.00;
 SimpleSchema.messages({
 	required: "[label] é obrigatório",
-	minString: "[label] must be at least [min] characters",
-	maxString: "[label] cannot exceed [max] characters",
-	minNumber: "[label] must be at least [min]",
-	maxNumber: "[label] cannot exceed [max]",
-	minDate: "[label] must be on or after [min]",
-	maxDate: "[label] cannot be after [max]",
-	badDate: "[label] is not a valid date",
-	minCount: "You must specify at least [minCount] values",
-	maxCount: "You cannot specify more than [maxCount] values",
-	noDecimal: "[label] must be an integer",
-	notAllowed: "[value] is not an allowed value",
-	expectedString: "[label] must be a string",
-	expectedNumber: "[label] must be a number",
-	expectedBoolean: "[label] must be a boolean",
-	expectedArray: "[label] must be an array",
-	expectedObject: "[label] must be an object",
-	expectedConstructor: "[label] must be a [type]",
+	minString: "[label] deve conter pelo menos [min] caracteres",
+	maxString: "[label] não pode ultrapassar [max] caracteres",
+	minNumber: "[label] deve ser um número maior que [min]",
+	maxNumber: "[label] deve ser um número menor que [max]",
+	minDate: "[label] deve ser uma data maior que [min]",
+	maxDate: "[label] deve ser uma data menor que [max]",
+	badDate: "[label] não é uma data válida",
+	minCount: "Você deve especificar ao menos [minCount] valores",
+	maxCount: "Você não deve especificar mais que [maxCount] valores",
+	noDecimal: "[label] deve ser um inteiro",
+	notAllowed: "[value] não é um valor válido para [label]",
+	expectedString: "[label] deve ser texto",
+	expectedNumber: "[label] deve ser um número",
+	expectedBoolean: "[label] deve ser um boolean",
+	expectedArray: "[label] deve ser um array",
+	expectedObject: "[label] deve ser um objeto",
+	expectedConstructor: "[label] deve ser do tipo [type]",
 	regEx: [
 		{msg: "[label] failed regular expression validation"},
-		{exp: SimpleSchema.RegEx.Email, msg: "[label] must be a valid e-mail address"},
-		{exp: SimpleSchema.RegEx.WeakEmail, msg: "[label] must be a valid e-mail address"},
-		{exp: SimpleSchema.RegEx.Domain, msg: "[label] must be a valid domain"},
-		{exp: SimpleSchema.RegEx.WeakDomain, msg: "[label] must be a valid domain"},
+		{exp: SimpleSchema.RegEx.Email, msg: "[label] deve ser um endereço de e-mail válido"},
+		{exp: SimpleSchema.RegEx.WeakEmail, msg: "[label] deve ser um endereço de e-mail válido"},
+		{exp: SimpleSchema.RegEx.Domain, msg: "[label] deve ser um domínio válido"},
+		{exp: SimpleSchema.RegEx.WeakDomain, msg: "[label] deve ser um domínio válido"},
 		{exp: SimpleSchema.RegEx.IP, msg: "[label] must be a valid IPv4 or IPv6 address"},
 		{exp: SimpleSchema.RegEx.IPv4, msg: "[label] must be a valid IPv4 address"},
 		{exp: SimpleSchema.RegEx.IPv6, msg: "[label] must be a valid IPv6 address"},
 		{exp: SimpleSchema.RegEx.Url, msg: "[label] must be a valid URL"},
 		{exp: SimpleSchema.RegEx.Id, msg: "[label] must be a valid alphanumeric ID"}
 	],
-	keyNotInSchema: "[key] is not allowed by the schema"
+	keyNotInSchema: "[key] não é permitido nesse schema"
 });
 if (Meteor.isClient) {
 	Session.setDefault('card_hash', 0);
@@ -49,6 +49,24 @@ if (Meteor.isClient) {
 		{name: 'ideal-2',  id: 21693,        valor: 'R$ 40.00', color: 'rgb(38, 53,  96)', active: false},
 		{name: 'outro',    id: 'outro-valor', valor: 'escolha outro valor', color: 'rgb(103, 58, 142)', active: false}
 	]);
+	Session.setDefault('dados_doacao', {
+		name            : '',
+		document_number : '',
+		born_at         : '',
+		gender          : '',
+		email           : '',
+		address : {
+			street        : '',
+			neighborhood  : '',
+			zipcode       : '',
+			street_number : '',
+			complementary : ''
+		},
+		phone : {
+			ddd    : 0,
+			number : 0
+		}
+	});
 	Meteor.startup(function() {
 		$('html').attr('lang', 'pt-BR');
 	});
@@ -98,6 +116,9 @@ if (Meteor.isClient) {
 		},
 		flash_message: function () {
 			return Session.get('flash_message');
+		},
+		dados_doacao: function () {
+			return Session.get('dados_doacao');
 		}
 	});
 
@@ -110,9 +131,11 @@ if (Meteor.isClient) {
 		}
 		return v;
 	};
+
 	var trocar_plano = function (plano_id) {
 			let planos = Session.get('planos');
 			Session.set('plano_ativo', plano_id);
+			Session.set('flash_message', '');
 			if (plano_id === 'outro-valor') { // ativa plano para planos maiores
 				Session.set('is_outro_plano_visible', true);
 				Session.set('is_form_details_validate', false);
@@ -128,6 +151,7 @@ if (Meteor.isClient) {
 
 	Template.doacao.events({
 		'click #voltar': function(evt) {
+			Session.set('flash_message', '');
 			Session.set('is_form_details_validate', false);
 			return false;
 		},
@@ -144,22 +168,22 @@ if (Meteor.isClient) {
 			evt.preventDefault();
 
 			TelefoneSchema = new SimpleSchema({
-				ddd    : {type: String},
-				number : {type: String}
+				ddd    : {type: Number, min: 2, label: 'DDD'},
+				number : {type: Number, min: 8, label: 'Telefone'}
 			});
 			EnderecoSchema = new SimpleSchema({
-				street        : {type: String},
-				neighborhood  : {type: String},
-				zipcode       : {type: String},
-				street_number : {type: String},
-				complementary : {type: String}
+				street        : {type: String, min: 0},
+				neighborhood  : {type: String, min: 0},
+				zipcode       : {type: Number, regEx: /^[0-9]{8}$/, label: 'CEP'},
+				street_number : {type: Number, label: 'Número'},
+				complementary : {type: String, label: 'Complemento'}
 			});
 			DadosPessoaisSchema = new SimpleSchema({
-				name            : {type: String, min: 1},
-				document_number : {type: String},
-				born_at         : {type: String},
-				gender          : {type: String},
-				email           : {type: String},
+				name            : {type: String, min: 5, label: 'Nome'},
+				email           : {type: String, regEx: SimpleSchema.RegEx.Email, label: 'E-mail'},
+				document_number : {type: Number, min: 11, label: 'CPF'},
+				born_at         : {type: Date, label: 'Data de nascimento'},
+				gender          : {type: String, allowedValues: ['F', 'M'], label: 'Gênero'},
 				address : {type: EnderecoSchema},
 				phone : {type: TelefoneSchema}
 			});
@@ -167,30 +191,36 @@ if (Meteor.isClient) {
 
 			var customer = {
 				name            : evt.target.customer_name.value,
-				document_number : evt.target.customer_document_number.value,
-				born_at         : evt.target.customer_born_at.value,
+				document_number : parseInt(evt.target.customer_document_number.value),
+				born_at         : new Date(evt.target.customer_born_at.value),
 				gender          : evt.target.customer_gender.value,
 				email           : evt.target.customer_email.value,
 				address : {
 					street        : evt.target.customer_address_street.value,
 					neighborhood  : evt.target.customer_address_neighborhood.value,
-					zipcode       : evt.target.customer_address_zipcode.value,
-					street_number : evt.target.customer_address_street_number.value,
+					zipcode       : parseInt(evt.target.customer_address_zipcode.value),
+					street_number : parseInt(evt.target.customer_address_street_number.value),
 					complementary : evt.target.customer_address_complementary.value
 				},
 				phone : {
-					ddd    : evt.target.customer_phone_ddd.value,
-					number : evt.target.customer_phone_number.value
+					ddd    : parseInt(evt.target.customer_phone_ddd.value),
+					number : parseInt(evt.target.customer_phone_number.value)
 				}
 			};
 
-			context = DadosPessoaisSchema.namedContext("myContext");
-			isValid = context.validate(customer);
+			ctxt = DadosPessoaisSchema.namedContext("myContext");
+			isValid = ctxt.validate(customer);
 
 			if (!isValid) {
-					console.log(context.invalidKeys());
+				var ik = ctxt.invalidKeys();
+				ik = _.map(ik, function (o) {
+					return _.extend({text: ctxt.keyErrorMessage(o.name)}, o);
+				});
+				Session.set('flash_message', {className: 'error', 'messages': ik});
 			} else {
 				Session.set('is_form_details_validate', 'valido');
+				Session.set('flash_message', '');
+				Session.set('dados_doacao', customer);
 			}
 		},
 		'submit #dados_pagamento_apoiador': function (evt) {
@@ -215,53 +245,31 @@ if (Meteor.isClient) {
 				//var list_errors = _.each(fieldErrors, function(v, k, list) { console.log(k,v); return k + ':' + v; })
 				var stringErrors = [];
 				for (var field in fieldErrors) {
-					stringErrors.push(fieldErrors[field]);
+					stringErrors.push({text:fieldErrors[field]});
 				}
 
 				Session.set('flash_message', {
-					className: 'alert-error alert',
-					text: 'Falta preencher informações. Operação não pode ser realizada. ' + stringErrors.join(" - ")
+					className: 'error',
+					messages: stringErrors
 				});
 			} else {
 				// se não há erros, gera o card_hash...
 				creditCard.generateHash(function(cardHash) {
 					Session.set('card_hash', cardHash);
-
-					var customer = {
-						name            : evt.target.customer_name.value,
-						document_number : evt.target.customer_document_number.value,
-						born_at         : evt.target.customer_born_at.value,
-						gender          : evt.target.customer_gender.value,
-						email           : evt.target.customer_email.value
-					};
-					var address = {
-						street        : evt.target.customer_address_street.value,
-						neighborhood  : evt.target.customer_address_neighborhood.value,
-						zipcode       : evt.target.customer_address_zipcode.value,
-						street_number : evt.target.customer_address_street_number.value,
-						complementary : evt.target.customer_address_complementary.value
-					};
-					var phone = {
-						ddd    : evt.target.customer_phone_ddd.value,
-						number : evt.target.customer_phone_number.value
-					};
-					var plan_id = evt.target.plan_id.value;
-
-					Meteor.call('assinar_plano', plan_id, cardHash, customer, address, phone, function(err, results) {
+					var customer = Session.get('dados_doacao');
+					var plan_id = Session.get('plano_ativo');
+					Session.set('flash_message', {className: 'loading', messages: [{text: ''}]});
+					Meteor.call('assinar_plano', plan_id, cardHash, customer, function(err, results) {
+							console.log(results, err);
 						if ( (typeof err === undefined) && (results.statusCode === 200) ) {
 							Session.set('flash_message', {
-								className: 'alert success',
-								text:'Assinatura realizada com sucesso.'
+								className: 'success',
+								messages: [{text:'Assinatura realizada com sucesso.'}]
 							});
 						} else {
-							var stringErrors = [];
-							// console.log(results, err);
-							// for (var field in results.errors) {
-							// 	stringErrors.push(results.errors[field].message);
-							// }
 							Session.set('flash_message', {
-								className: 'alert error',
-								text:'Falha ao tentar realizar a assinatura.' + stringErrors.join(" - ")
+								className: 'error',
+								messages: [{text:'Falha ao tentar realizar a assinatura. ' + err.message}]
 							});
 						}
 					});
@@ -274,16 +282,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 Meteor.methods({
-	'assinar_plano': function (plan_id, card_hash, customer, address, phone) {
+	'assinar_plano': function (plan_id, card_hash, customer) {
 		return HTTP.call("POST", "https://api.pagar.me/1/subscriptions", {
 				data: {
-					api_key: Meteor.settings.PagarMe.API_KEY,
-					plan_id: plan_id,
-					card_hash: card_hash,
-					customer: _.extend(customer, {
-						address: address,
-						phone: phone
-					})
+					'api_key': Meteor.settings.PagarMe.API_KEY,
+					'plan_id': plan_id,
+					'card_hash': card_hash,
+					'customer': customer
 				}
 			});
 	},
